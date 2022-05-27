1c7f6d93ae0d6cab48a0d1067667c7b68f81f6ce
ВинБоцы ВинБоцы <solarisraver@mail.ru>
2022-05-27 21:13:59 +0600

    HomeWork_2 DONE

--- HomeWork_2/Program.cs
+++ HomeWork_2/Program.cs
@@ -4,6 +4,19 @@ namespace HomeWork_2
 {
     class Program
     {
+        public static void MinMax(int[,] Arr, out int min, out int max)
+        {
+            min = Arr[0, 0];
+            max = Arr[0, 0];
+            for (int i = 0; i < Arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < Arr.GetLength(1); j++)
+                {
+                    if (min > Arr[i, j]) min = Arr[i, j];
+                    if (max < Arr[i, j]) max = Arr[i, j];
+                }
+            }
+        }
         static void Main(string[] args)
         {
             //Task 1, 2
@@ -38,51 +51,87 @@ namespace HomeWork_2
             //Console.WriteLine($"{number} встречается {count} раз.");
 
             //Task 4
-            int M = 4;
-            int[] arr = new int[M];
-            Console.WriteLine($"Введите эелементы первого массива: ");
-            for (int i = 0; i < arr.Length; i++) arr[i] = int.Parse(Console.ReadLine());
-            Console.WriteLine($"Первый массив: ");
-            foreach (int num in arr) Console.Write($"{num}\t");
-            int N = 4;
-            int[] brr = new int[N];
-            Console.WriteLine($"\nВведите эелементы второго массива: ");
-            for (int i = 0; i < brr.Length; i++) brr[i] = int.Parse(Console.ReadLine());
-            Console.WriteLine($"Второй массив: ");
-            foreach (int num in brr) Console.Write($"{num}\t");
-            int count = 0;
-            for (int i = 0; i < M; i++)
+            //const int M = 4;
+            //const int N = 4;
+            // int[] arr = new int[M];
+            // Console.WriteLine($"Введите эелементы первого массива: ");
+            // for (int i = 0; i < arr.Length; i++) arr[i] = int.Parse(Console.ReadLine());
+            // Console.WriteLine($"Первый массив: ");
+            // foreach (int num in arr) Console.Write($"{num}\t");
+            // int[] brr = new int[N];
+            // Console.WriteLine($"\nВведите эелементы второго массива: ");
+            // for (int i = 0; i < brr.Length; i++) brr[i] = int.Parse(Console.ReadLine());
+            // Console.WriteLine($"Второй массив: ");
+            // foreach (int num in brr) Console.Write($"{num}\t");
+            // int count = 0;
+            // for (int i = 0; i < M; i++)
+            // {
+            //     for (int j = 0; j < N; j++)
+            //     {
+            //         if (arr[i] == brr[j]) count++;
+            //     }
+            // }
+            // Console.Write($"\nКоличество совпадений: {count}");
+            // int p = count;
+            // int[] crr = new int[p];
+            // p = 0;
+            // for (int j = 0; j < N; j++)
+            // {
+            //     for (int k = 0; k < M; k++)
+            //     {
+            //         if (brr[j] == arr[k])
+            //         {
+            //             crr[p] = brr[j];
+            //             p++;
+            //         }
+            //     }
+            // }
+            // Console.WriteLine($"\nРезультирующий масиив: ");
+            // for (int i = 0; i < p; i++) Console.Write($"{crr[i]}\t");
+
+            //Task 5
+            //int[,] Arr = new int[,] { { 1, 2, 3, 4 }, { 5, 6, 7, 8 } };
+            //MinMax(Arr, out int min, out int max);
+            //Console.WriteLine($"Миримальное значение массива: {min}");
+            //Console.WriteLine($"Максимальное значение массива: {max}");
+
+            //Task 6, 7, 8
+            //Console.WriteLine($"Введите строку: ");
+            //string str = Console.ReadLine();
+            //Console.WriteLine($"str: {str}");
+            //string[] strArr = str.Split(' ');
+            //Console.WriteLine($"Количество слов в строке: {strArr.Length}");
+            //Console.WriteLine($"Реверсирование строки: ");
+            //for (int i = str.Length - 1; i >= 0; --i) Console.Write(str[i]);
+            //string str_l = str.ToLower();
+            //int count = 0;
+            //for (int i = 0; i < str_l.Length; i++)
+            //{
+            //    if
+            //        (
+            //        str_l.Contains("а") || str_l.Contains("е") || str_l.Contains("ё") || str_l.Contains("и") || str_l.Contains("о") ||
+            //        str_l.Contains("у") || str_l.Contains("ы") || str_l.Contains("э") || str_l.Contains("ю") || str_l.Contains("я")
+            //        )
+            //        count++;
+            //}
+            //Console.WriteLine($"\nКоличество гласных букв в строке: {count}");
+
+            //Task 9
+            Console.WriteLine($"Введите строку: ");
+            string str1 = Console.ReadLine();
+            Console.WriteLine($"Введите слова для поиска: ");
+            string str2 = Console.ReadLine();
+            int i = 0;
+            int x = -1;
+            int count = -1;
+            while (i != -1)
             {
-                for (int j = 0; j < N; j++)
-                {
-                    if (arr[i] == brr[j]) count++;
-                }
+                i = str1.IndexOf(str2, x + 1);
+                x = i;
+                count++;
             }
-            int p = (M + N) - count;
-            int[] crr = new int[p];
-            int item = 0;
-            for (int i = 0; i < M; i++)
-            {
-                    crr[item] = arr[i];
-                    item++;
-            }
-            int pov = 0;
-            int index = 0;
-            for (int i = 0; i < M; i++)
-            {
-                for(int j = 0; j < N; j++)
-                {
-                    if (arr[i] == brr[j]) pov++;
-                }
-                if (pov == 0)
-                {
-                    crr[item] = brr[index];
-                    item++;
-                    index++;
-                }
-            }
-            Console.WriteLine($"\nРезультирующий масиив: ");
-            for (int i = 0; i < p; i++) Console.Write($"{crr[i]}\t");
+            Console.Write($"\nКоличество найденных слов: {count}");
+
         }
     }
 }
